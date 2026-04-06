3662627311900d0349c817873cdeb4c86349c70e
shahriar021 <shahriarc19@gmail.com>
2026-04-06 20:51:29 +0600

    feat:add home screen design with data

---
+++ src/redux/features/home/homeApi.ts
@@ -0,0 +1,17 @@
+import { baseApi } from "src/redux/createdApi/baseApi"
+
+const homeApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getIndustries: builder.query({
+      query: () => " /api/industry/get", 
+    }),
+    getRecommendedJobs: builder.query({
+      query: () => "/api/job/get",
+    }),
+    getPopularCompanies: builder.query({
+      query: () => "/api/company/get",
+    }),
+  }),
+});
+
+export const { useGetIndustriesQuery, useGetRecommendedJobsQuery, useGetPopularCompaniesQuery } = homeApi;
